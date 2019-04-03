793c45bfae3fd1728b11635deb8eb1718c2be191
himanshu shukla <himanshushukla16398@gmail.com>
2019-04-03 08:22:38 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material'; 
+import { HeaderComponent } from './header/header.component';
+import { AddComponent } from './add/add.component';
+import { CustomerinfoComponent } from './customerinfo/customerinfo.component';
+import {RouterModule,Routes} from '@angular/router';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AddComponent,
+    CustomerinfoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
